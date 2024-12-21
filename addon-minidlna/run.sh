765a4796b115925a2b8c4d7e2a2c4989088ac476
Thomas Kopp <thomas.kopp@online.de>
2024-12-21 20:13:06 +0100

    -

--- addon-minidlna/run.sh
+++ addon-minidlna/run.sh
@@ -10,19 +10,31 @@ CONFIG="/etc/minidlna.conf"
 bashio::log.info "Creating MiniDLNA configuration ${CONFIG}"
 
 # Declare variables
+declare port
 declare media_dir
+declare album_art_names
 declare inotify
+declare enable_tivo
+declare tivo_discovery
+declare strict_dlna
+declare notify_interval
+declare serial
+declare model_number
 
-
-# Create main config
-media_dir_A=$(bashio::config "A,/media/music")
-inotify=$(bashio::config 'no')                                   # 'no' for less resources, restart required for new media
 
 {
-  echo "media_dir=$(bashio::config 'media_dir_A')"
+  echo "port=$(bashio::config 'port')"
+  echo "media_dir=$(bashio::config 'media_dir')"
+  echo "album_art_names=$(bashio::config 'album_art_names')"
   echo "inotify=$(bashio::config 'inotify')"
-} >> "${CONFIG}"
-
+  echo "enable_tivo=$(bashio::config 'enable_tivo')"
+  echo "tivo_discovery=$(bashio::config 'tivo_discovery')"
+  echo "strict_dlna=$(bashio::config 'strict_dlna')"
+  echo "notify_interval=$(bashio::config 'notify_interval')"
+  echo "serial=$(bashio::config 'serial')"
+  echo "model_number=$(bashio::config 'model_number')"
+  echo "=$(bashio::config '')"
+} > "${CONFIG}"
 
 bashio::log.info "Starting MiniDLNA service"
 exec minidlnad  
