382f1c4d5917f6de72c15273efb0cfb972d4c55d
Thomas Kopp <thomas.kopp@online.de>
2025-10-13 15:57:11 +0000

    update

--- addon-mdns-example/run.sh
+++ addon-mdns-example/run.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-# run.sh – Minimal mDNS Test Addon
-
-set -e
-
+# run.sh
 DEVICE_NAME="${DEVICE_NAME:-mdns-test-addon}"
 PORT="${PORT:-8080}"
 
 echo "ℹ️ Starte mDNS Service für $DEVICE_NAME auf Port $PORT..."
 
-# Starte Python-Skript, das mDNS registriert
 python3 main.py "$DEVICE_NAME" "$PORT"
