c70df23cbeb921325b0df11a54bdcba92b1a4791
Thomas Kopp <thomas.kopp@online.de>
2025-10-13 11:20:13 +0000

    update

--- addon-mdns-example/run.sh
+++ addon-mdns-example/run.sh
@@ -4,20 +4,5 @@
 # ------------------------------
 set -e
 
-echo "ℹ️ Starte mDNS Test Addon..."
-
-# Optional: DBus + Avahi (wenn Host-Avahi nicht genutzt wird)
-# apk add py3-dbus avahi avahi-tools bereits im Dockerfile
-if command -v dbus-daemon >/dev/null 2>&1; then
-    echo "ℹ️ Starte DBus-Daemon..."
-    dbus-daemon --system &
-fi
-
-if command -v avahi-daemon >/dev/null 2>&1; then
-    echo "ℹ️ Starte Avahi-Daemon..."
-    avahi-daemon --no-chroot &
-fi
-
-# Python Skript starten
-echo "ℹ️ Starte Python mDNS Service..."
+echo "ℹ️ Starte mDNS Service..."
 python3 /app/main.py
